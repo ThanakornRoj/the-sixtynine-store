95e28d913544f22d67b06c2bfe26ed5579c4859a
Thanakorn <thanakorn.rojnakarint@gmail.com>
2021-02-09 13:29:34 +0700

    create manage admin page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
 import ManageUser from "../pages/member/mangeUser";
 import MemberAction from "../pages/member/memberAction";
+import ManageAdmin from "../pages/member/menageAdmin";
 
 const Router = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Router = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/manage-user" component={ManageUser} />
       <Route exact path="/member-action" component={MemberAction} />
+      <Route exact path="/manage-admin" component={ManageAdmin} />
     </Switch>
   </BrowserRouter>
 );
